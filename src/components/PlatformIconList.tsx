b616ad94f786361e197a108dc36a19a560b6a877
Marcin Otto <ottomarcin96@gmail.com>
2023-07-28 14:09:42 +0200

    feat: create components/GameCard

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import { Platform } from '../hooks/useGames';
+import { HStack, Icon } from '@chakra-ui/react';
+import {
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from 'react-icons/fa';
+
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { IconType } from 'react-icons';
+
+interface Props {
+  //   game: Game;
+  platforms: Platform[];
+}
+
+const iconMap: { [key: string]: IconType } = {
+  android: FaAndroid,
+  ios: MdPhoneIphone,
+  linux: FaLinux,
+  mac: FaApple,
+  nintendo: SiNintendo,
+  pc: FaWindows,
+  playstation: FaPlaystation,
+  web: BsGlobe,
+  xbox: FaXbox,
+};
+
+const PlatformIconList = ({ platforms }: Props) => {
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500' />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
